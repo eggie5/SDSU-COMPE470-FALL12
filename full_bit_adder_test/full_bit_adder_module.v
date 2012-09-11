f707b694a0fd7837007aa0f83d540e09164f2b6c
Alex Egg <eggie5@gmail.com>
2012-09-10 21:21:44 -0700

    Update full_bit_adder_test/full_bit_adder_module.v

--- full_bit_adder_test/full_bit_adder_module.v
+++ full_bit_adder_test/full_bit_adder_module.v
@@ -1,10 +1,16 @@
-
-module full_bit_adder_module(Cout, sum, lna, lnb);
-input lna, lnb;
+module full_bit_adder_module(Cout, sum, Cin, a, b);
+input Cin, a, b;
 output Cout, sum;
 
-xor (sum, lna, lnb);
-and (Cout, lna, lnb);
+xor (Cin, sum);
+
+//this is half adder 1
+xor (sum, a, b);
+and (Cout, a, b);
+
+//this is half adder 2
+xor (sum, a, b);
+and (Cout, a, b);
 
 
 endmodule
