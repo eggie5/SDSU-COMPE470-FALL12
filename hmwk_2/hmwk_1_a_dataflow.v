336da5ac1f1579a44009648bc3ddf3de70feb1e1
Alex egg <eggie5@gmail.com>
2012-09-24 22:01:05 -0700

    added hmwk 2

---
+++ hmwk_2/hmwk_1_a_dataflow.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module one(
+    input x3,
+    input x2,
+    input x1,
+    input x0,
+    output y1,
+    output y0
+    );
+	 
+	 not(x2_, x2);
+	 nand(y1, x3, x2);
+	 nand(y0, x0, x1, x2_);
+
+
+endmodule
