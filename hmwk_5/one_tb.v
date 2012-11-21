c95e36c620e582ca9d24f3b7bcf175d89ce398f5
Alex Egg <eggie5@gmail.com>
2012-11-20 20:21:53 -0800

    reading test vectors from binary file

--- hmwk_5/one_tb.v
+++ hmwk_5/one_tb.v
@@ -5,16 +5,25 @@ reg [3:0] X;
 reg clk;
 wire [3:0] Y;
 
+reg [7:0] mem[0:5];
+integer i;
+
 one uut(X, clk, Y);
 
+initial $readmemb("input.txt", mem);
+
 initial begin
+	
 	$display("time\t clk\t X\t Y");
 	$monitor("%g\t %d\t %d\t %d", $time, clk, X, Y);	
 	
 	clk=0;
 	X=0;
 	
-	#10 $finish;
+	for (i=0; i<8; i=i+1)
+		@(posedge clk) X = mem[i];
+	
+	@(posedge clk) $finish;
 end
 
 always #1 clk=~clk;
